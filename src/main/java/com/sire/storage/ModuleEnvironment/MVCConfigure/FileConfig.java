a9e67e2c1b04446986908ff0a6bed82814a233b1
wangkai <wangkai@yapingguo.com>
2018-02-09 16:58:26 +0800

    multipartResolver 注入错误，暂时注释掉

--- src/main/java/com/sire/storage/ModuleEnvironment/MVCConfigure/FileConfig.java
+++ src/main/java/com/sire/storage/ModuleEnvironment/MVCConfigure/FileConfig.java
@@ -14,13 +14,13 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
  */
 @Configuration
 public class FileConfig {
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
-        resolver.setDefaultEncoding("UTF-8");
-        resolver.setResolveLazily(true);// resolveLazily属性启用是为了推迟文件解析
-        resolver.setMaxInMemorySize(40960);
-        resolver.setMaxUploadSize(2 * 1024 * 1024);// 上传文件大小 2M 50*1024*1024
-        return resolver;
-    }
+//    @Bean()
+//    public CommonsMultipartResolver multipartResolver() {
+//        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+//        resolver.setDefaultEncoding("UTF-8");
+//        resolver.setResolveLazily(true);// resolveLazily属性启用是为了推迟文件解析
+//        resolver.setMaxInMemorySize(40960);
+//        resolver.setMaxUploadSize(2 * 1024 * 1024);// 上传文件大小 2M 50*1024*1024
+//        return resolver;
+//    }
 }
